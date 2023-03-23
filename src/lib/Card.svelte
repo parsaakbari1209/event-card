2bb19527951d9e2fbb70783ff4b2e5b5e0be4e1f
parsaakbari1209 <parsaakbari1209@gmail.com>
2023-03-23 20:33:38 +0330

    feat: add card component

---
+++ src/lib/Card.svelte
@@ -0,0 +1,64 @@
+<script lang="ts">
+  export let heading: string
+  export let styles: {
+    background: string
+    rotation: string
+    translationX: string
+    translationY: string
+  }
+  $: cssVarStyles = Object.entries(styles)
+    .map(([key, value]) => `--${key}:${value}`)
+    .join(';')
+</script>
+
+<div style={cssVarStyles}>
+  <h1>{heading}</h1>
+  <p>
+    Rent <span>any</span> <span class="circle-letter">P</span> <span>Proof Collective</span> NFT for <span>1 day</span>
+    for <span>0.0.5 ETH</span>
+  </p>
+</div>
+
+<style>
+  .circle-letter {
+    align-items: center;
+    background-color: #000;
+    border-radius: 50%;
+    box-sizing: border-box;
+    display: flex;
+    height: 22px;
+    justify-content: center;
+    width: 22px;
+  }
+  div {
+    background-color: var(--background);
+    border-radius: 5px;
+    display: flex;
+    flex-direction: column;
+    font-family: Arial;
+    height: 150px;
+    justify-content: space-between;
+    position: absolute;
+    transform: rotate(var(--rotation)) translate(var(--translationX), var(--translationY));
+    width: 400px;
+  }
+  h1 {
+    color: #333;
+    font-size: 20px;
+    font-weight: 700;
+    height: 30px;
+    margin: 10px;
+  }
+  p {
+    color: #333;
+    display: flex;
+    font-size: 14px;
+    font-weight: 400;
+    height: 20px;
+    justify-content: space-around;
+    margin: 10px;
+  }
+  span {
+    color: #fff;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,41 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import Card from '../lib/Card.svelte'
+  const cards = [
+    {
+      heading: 'Expecting an airdrop this summer Board Apache and other stuff...',
+      styles: { background: '#44eab1', rotation: '6deg', translationX: '0px', translationY: '-86px' },
+    },
+    {
+      heading: 'Want to buy a ticket to VeeCon?',
+      styles: { background: '#ff790f', rotation: '0deg', translationX: '-29px', translationY: '-43px' },
+    },
+    {
+      heading: 'Want to preview the PROOF Discord before buying a membership?',
+      styles: { background: '#9d97e0', rotation: '-9deg', translationX: '0px', translationY: '0px' },
+    },
+  ]
+</script>
+
+<svelte:head>
+  <title>Event Cards</title>
+</svelte:head>
+
+<div>
+  {#each cards as card}
+    <Card heading={card.heading} styles={card.styles} />
+  {/each}
+</div>
+
+<style>
+  :global(body) {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+  div {
+    align-items: center;
+    display: flex;
+    height: 100vh;
+    justify-content: center;
+  }
+</style>
